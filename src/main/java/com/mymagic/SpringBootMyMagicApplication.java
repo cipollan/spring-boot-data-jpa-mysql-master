82739fb31edc55ab297ad9062a0062929768d730
cipollan <cipollan@gmail.com>
2022-09-04 19:16:34 +0200

    Second Commitment

--- src/main/java/com/mymagic/SpringBootMyMagicApplication.java
+++ src/main/java/com/mymagic/SpringBootMyMagicApplication.java
@@ -1,9 +1,13 @@
 package com.mymagic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.mymagic.configuration.CachingConfig;
 
 
 @EnableCaching
@@ -11,8 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class SpringBootMyMagicApplication 
 {
+	private static  Logger log = LoggerFactory.getLogger(SpringBootMyMagicApplication.class);
 	public static void main(String[] args) 
 	{
+		log.info("+-------------- <CacheManager studentInfo> -----------------------+" );
 		SpringApplication.run(SpringBootMyMagicApplication.class, args);
 	}
  
